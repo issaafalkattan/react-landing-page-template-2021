d3b9d4c4482ac3d5076e483e31f461c379c231d9
codingconquerors <100064563+codingconquerors@users.noreply.github.com>
2022-02-22 21:17:40 -0500

    Update Header.tsx
    Fix for responsive design. The issue is menu link return 404 err in mobile screen mode.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -99,13 +99,17 @@ const Menu = () => {
               </div>
               <div className="px-2 pt-2 pb-3 space-y-1">
                 {navigation.map((item) => (
-                  <a
+                  <Link
+                    spy={true}
+                    active="active"
+                    smooth={true}
+                    duration={1000}
                     key={item.name}
-                    href={item.href}
+                    to={item.href}
                     className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <a
