99e2e0ecfd0cb0d7820f2a749d5616bbb9d9fb73
ario@riz.ky <rizkyario@me.com>
2022-06-18 11:10:59 +0200

    Add retrovrs theme

--- src/components/Wave.tsx
+++ src/components/Wave.tsx
@@ -10,8 +10,9 @@ const Wave: FC = () => {
   const height = 600;
   let frequency = 0.013;
   const waves = {
-    frontWave: new WaveObj([0.0211, 0.028, 0.015], 'rgb(236, 71, 85, 0.1)'),
-    backWave: new WaveObj([0.0122, 0.018, 0.005], 'rgb(249, 168, 168, 0.1)'),
+    frontWave: new WaveObj([0.0211, 0.028, 0.015], 'rgb(129, 177, 250, 0.8)'),
+    middleWave: new WaveObj([0.0182, 0.022, 0.01], 'rgb(180, 176, 250, 0.8)'),
+    backWave: new WaveObj([0.0122, 0.018, 0.005], 'rgb(237, 174, 249, 0.8)'),
   };
 
   const render = () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#ec4755',
-        secondary: '#a12c34',
+        primary: '#7B61FF',
+        secondary: '#B4B0FA',
         tertiary: '#99a0a3',
         border: '#1a2e35',
         background: '#ffffff',
