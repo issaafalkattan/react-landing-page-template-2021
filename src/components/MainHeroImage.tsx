161388c7490edda2bbacf8f73830e1ac3136664e
ario@riz.ky <rizkyario@me.com>
2022-06-18 15:40:03 +0200

    Fix MainHeroImage

--- src/components/MainHeroImage.tsx
+++ src/components/MainHeroImage.tsx
@@ -7,7 +7,8 @@ const MainHeroImage = () => {
   return (
     <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
       <img
-        className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+        className="w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+        style={{ maxWidth: '40rem' }}
         src={mainHero.img}
         alt="happy team image"
       />
