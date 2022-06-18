d5a300c5a3949b57e1bbb80586b7c4bd13b2fc25
Rizky Ario <ario@riz.ky>
2022-06-18 15:20:39 +0200

    Update index.json

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -8,8 +8,9 @@ const MainHero = () => {
     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
       <div className="sm:text-center lg:text-left">
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-          <span className="block xl:inline">{mainHero.title}</span>{' '}
-          <span className={`block text-primary xl:inline`}>
+          <span className="block xl:inline">{mainHero.title}</span>
+          <br />
+          <span className={`block text-primary xl:inline text-3xl`}>
             {mainHero.subtitle}
           </span>
         </h1>
