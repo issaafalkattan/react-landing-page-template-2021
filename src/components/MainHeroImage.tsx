8e7d5224d58c3d9319f2927484b1e494b9233a53
ario@riz.ky <rizkyario@me.com>
2022-06-18 23:57:23 +0200

    Fix MainHeroImage

--- src/components/MainHeroImage.tsx
+++ src/components/MainHeroImage.tsx
@@ -5,9 +5,16 @@ import config from '../config/index.json';
 const MainHeroImage = () => {
   const { mainHero } = config;
   return (
-    <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+    <div
+      className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <img
-        className="w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+        className="w-full object-cover lg:w-full lg:h-full"
         style={{ maxWidth: '40rem' }}
         src={mainHero.img}
         alt="happy team image"
