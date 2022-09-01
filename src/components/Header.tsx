514bfa6ddbd842c4cdb928b1402258cf55cfebc7
AD Mohammad <62221590+advista216@users.noreply.github.com>
2022-08-31 19:11:43 -0600

    1
    1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -62,7 +62,7 @@ const Menu = () => {
                 href="#"
                 className={`font-medium text-primary hover:text-secondary`}
               >
-                Call to action
+                {/* Your Impact */}
               </a>
             </div>
           </nav>
